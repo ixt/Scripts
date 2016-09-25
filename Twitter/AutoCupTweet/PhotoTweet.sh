99f95de56e6d13c4c6ecc7075caed52abe2ee8a2
orange <orange@ff4500.red>
2016-09-25 00:54:41 +0000

    changed banner placement for readability

--- Twitter/AutoCupTweet/PhotoTweet.sh
+++ Twitter/AutoCupTweet/PhotoTweet.sh
@@ -3,5 +3,5 @@
 NAME=$(date +%c)
 FILENAME=$(date -Iseconds | sed s/\ /_/g | sed s/\:/-/g)
 
-fswebcam -r 1920x1080 --font sans:50 --banner-colour "#FF000000" --line-colour "#FFFF4500" --no-shadow -S 300 --jpeg 100 "/home/chip/photos/dreher/$FILENAME.jpg" 
+fswebcam -r 1920x1080 --font sans:50 --top-banner --banner-colour "#FF000000" --line-colour "#FFFF4500" --no-shadow -S 300 --jpeg 100 "/home/chip/photos/dreher/$FILENAME.jpg" 
 t update "$NAME #drehertweet" -f "/home/chip/photos/dreher/$FILENAME.jpg"
