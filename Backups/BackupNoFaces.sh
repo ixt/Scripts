f2b69d7192fc0148283a8bfa073beef59be4fc16
ixt <orange@ff4500.red>
2016-10-10 06:58:04 +0100

    File path changes in Nofaces

--- Backups/BackupNoFaces.sh
+++ Backups/BackupNoFaces.sh
@@ -3,10 +3,10 @@
 
 BACKUPPLACE=/home/orange/Desktop/photos/NoFacesForNames/
 NOFACEDIR=/home/orange/Projects/NoFacesForNames
-BEGINNING=$(ls $NOFACEDIR/images/ -1 | tail -1 | sed -e 's/\///g')
-END=$(ls $NOFACEDIR/images/ -1 | head -1 | sed -e 's/\///g')
+BEGINNING=$(ls $NOFACEDIR/files/ -1 | tail -1 | sed -e 's/\///g')
+END=$(ls $NOFACEDIR/files/ -1 | head -1 | sed -e 's/\///g')
 TARNAME=$(echo $BEGINNING"-"$END".tar.gz")
 
-#tar -czvf $NOFACEDIR/$TARNAME $NOFACEDIR/images/*
-rm $NOFACEDIR/images/* -r
-#mv $NOFACEDIR/$TARNAME $BACKUPPLACE
+tar -czvf $NOFACEDIR/$TARNAME $NOFACEDIR/files/*
+rm $NOFACEDIR/files/* -r
+mv $NOFACEDIR/$TARNAME $BACKUPPLACE
