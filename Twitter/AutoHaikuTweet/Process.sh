92673f9d49db7ce074b4cca139fab781aefdad01
ixt <orange@ff4500.red>
2016-10-18 11:52:08 +0100

    cleaning
    Signed-off-by: ixt <orange@ff4500.red>

--- Twitter/AutoHaikuTweet/Process.sh
+++ Twitter/AutoHaikuTweet/Process.sh
@@ -2,23 +2,24 @@
 
 # Gets a random english wikipedia article and cuts it up as a haiku for tweeting
 
-HAIKULOC="/home/chip/605204/Haiku.py"
+HOMEDIR="/home/orange/Projects"
+HAIKULOC=$(echo "$HOMEDIR""/605204/Haiku.py")
+OUTPUTS=$(echo "$HOMEDIR""/Scripts/Twitter/AutoHaikuTweet/outputs")
 SOURCE='https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&redirects=1&generator=random'
-OUTPUTS="/home/chip/Scripts/Twitter/AutoHaikuTweet/outputs"
 AHAIKU=1
 
 while [ $AHAIKU -eq 1 ]; do
     lynx --dump -width 1024 $SOURCE > .tempsite
-    cat .tempsite | sed -e 's/.*"extract":"//' | sed -e 's/<[^<>]*>//g' | sed -e 's/([^()]*)//g' > .tempwhile
-    TITLE=$(cat .tempsite | sed -e 's/.*title\"\:\"//g' | sed 's/:punct://g' | sed -e 's/\".*/\:\n/g' )
-    URL=$(echo https://en.wikipedia.org/wiki/$(cat .tempsite | sed -e 's/.*title\"\:\"//g' | sed 's/:punct://g' | sed -e 's/ /\_/g' | sed -e 's/\".*/\n/g' ) )
-    echo $TITLE > .title
-    cat .tempwhile | ascii2uni -a U -q | uni2ascii -q -B | sed -e 's/\\n/\n/g' | sed -e 's/\\//g' | sed -e 's/[0-9]//g' > .tempin
+    cat .tempsite | sed -e 's/.*"extract":"//g;s/<[^<>]*>//g;s/([^()]*)//g' > .tempwhile
+    TITLE=$(cat .tempsite | sed -e 's/.*title\"\:\"//g;s/:punct://g;s/\".*/\:\n/g' )
+    echo $TITLE | ascii2uni -a U -q | uni2ascii -q -B | sed -e 's/\\n/\n/g;s/\\//g;s/[0-9]//g'> .title
+    URL=$(echo https://en.wikipedia.org/wiki/$(cat .title | sed -e 's/ /_/g;s/\://g') )
     echo $URL > .url
+    cat .tempwhile | ascii2uni -a U -q | uni2ascii -q -B | sed -e 's/\\n/\n/g;s/[0-9|\\]//g' > .tempin
     $HAIKULOC -i .tempin -o .tempout > $OUTPUTS/lasttweet.txt
     HAIKU=$(cat $OUTPUTS/lasttweet.txt)
     NOTHAIKU=$(cat $OUTPUTS/notenoughwords.txt)
-    if grep -q "User\|Talk" .title; then
+    if grep -q "User\|Talk\|File\|Title" .title; then
         echo "wah"
     else
         if [ "$HAIKU" != "$NOTHAIKU" ]; then
@@ -26,10 +27,9 @@ while [ $AHAIKU -eq 1 ]; do
         fi
     fi
 done;
-echo $TITLE > .title
 cat .title $OUTPUTS/lasttweet.txt .url > .temphere
 cat .temphere
 cat .temphere > "$OUTPUTS/$TITLE.txt"
-t update "$(cat .temphere)"
+#t update "$(cat .temphere)"
 rm .tempin .tempout .tempsite .temphere .tempwhile .title .url
 echo "DONE!"
