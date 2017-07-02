5fa009e604ebdffc0f1f0e80a1e949d351471d7d
NfN Orange <orange@ff4500.red>
2017-07-02 00:46:00 +0000

    updated font

--- Twitter/ClockFace/ClockFace.sh
+++ Twitter/ClockFace/ClockFace.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 FILEDIR=$(dirname $0)
-convert -background black -fill orangered -size 500x500 -pointsize 160 -gravity center label:`date -u +%H:%M` $FILEDIR/output.png
+convert -background black -fill orangered -size 500x500 -font /usr/share/fonts/truetype/dseg/DSEG7Classic-BoldItalic.ttf -pointsize 100 -gravity center label:`date -u +%H:%M` $FILEDIR/output.png
 t set profile_image $FILEDIR/output.png 
