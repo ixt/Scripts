0e6177ed58da7574bd75f00a238c78a3fe5b5d24
NfN Orange <orange@ff4500.red>
2017-11-02 14:21:14 +0000

    Progress bar in bash

---
+++ Misc/BashNotes/02-ProgressBar.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+START=0
+END=333
+COLUMNS=$(bc -l <<< "$(tput cols) - 10")
+BLANK=""
+for block in `seq 1 $((COLUMNS + 10))`; do
+    BLANK="${BLANK} "
+done
+for i in `seq $START $END`; do
+    STRING="[ "
+    sleep 0.001s
+    PERCENT=$(bc -l <<< "($i / $END) * 100" | cut -d. -f1 | xargs printf %03d )
+    BLOCKS=$(bc -l <<< "$COLUMNS * ($PERCENT / 100)" | cut -d. -f1)
+    for block in `seq 1 $BLOCKS`; do
+        STRING="${STRING}#"
+    done
+    for block in `seq 1 $(( COLUMNS - BLOCKS ))`; do
+        STRING="${STRING} "
+    done
+    echo -ne "\b$BLANK" 
+    echo -e "\r$PERCENT%"
+    echo -ne "\b$STRING ]$PERCENT% ]\r" 
+done
+echo ""
