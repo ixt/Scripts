fa37215bbfe6639ba420188b577db87a68e0d598
NfN Orange <orange@ff4500.red>
2017-11-14 15:10:23 +0000

    Fixing proper checking for mr rogers button

--- Misc/MrRogersButton/Button.sh
+++ Misc/MrRogersButton/Button.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+PROCESS=""
 play() {
 	VAL=$(ls -1 *.mkv | shuf | head -1)
-	screen -dm mpv $VAL --no-video
+	screen -dm mpv $VAL --no-video 
+	PROCESS=$(ps -aux | grep -e "mpv" | grep video | xargs echo | cut -d" " -f2 | head -1)
 	echo "Playing $VAL"
 }
 
@@ -10,9 +12,12 @@ while true; do
 	if [ "$(cat /sys/class/gpio/gpio23/value)" -eq "0" ]; then 
 		echo 1 > /sys/class/gpio/gpio25/value
 		play 
+		while [ "$(cat /sys/class/gpio/gpio23/value)" -eq "0" ]; do
+			sleep 1s
+		done
 		sleep 1s
-		while wait; do
-			[ "$(cat /sys/class/gpio/gpio23/value)" -eq "0" ] && pkill mpv && break
+		while [ $(ps -p $PROCESS | wc -l) -eq "2" ]; do
+			[ "$(cat /sys/class/gpio/gpio23/value)" -eq "0" ] && pkill mpv
 			sleep 0.1s
 		done
 		echo 0 > /sys/class/gpio/gpio25/value 
