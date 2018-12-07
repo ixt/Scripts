f52ebe2fa87e7994ed2fa7e22f04b7c0549f85c5
NfN Orange <orange@ff4500.red>
2018-12-07 02:05:49 +0000

    Fixing some issues with tcpdump scraping and chaning how the IPinCountry works

--- Webscraping/IPinCountryOut.sh
+++ Webscraping/IPinCountryOut.sh
@@ -7,24 +7,25 @@ if [[ -f "IPDB.json" ]]; then
     echo "," >> $TEMP
 fi
 [[ ! -f "IPDB.json" ]] && echo "{" > $TEMP
-while read IP; do
+#while read IP; do
+IP=$1
     if grep -q "\<${IP}\>" $TEMP; then
-        echo "Already there" >&2
+        # echo "Already there" >&2
         STRING=".[\"${IP}\"]"
         sed -i "$ d" $TEMP
         echo "}" >> $TEMP
-        jq "$STRING" $TEMP > $OBJECTTEMP
+        jq "$STRING" $TEMP 2>/dev/null > $OBJECTTEMP
         sed -i "$ d" $TEMP
         echo "," >> $TEMP
     else
         echo "\"${IP}\": " >> $TEMP
-        curl "https://api.ip2country.info/ip?${IP}" | tee -a $TEMP > $OBJECTTEMP
+        curl -s "https://api.ip2country.info/ip?${IP}" | tee -a $TEMP > $OBJECTTEMP
         echo "," >> $TEMP
     fi
-    NAME=$(jq -r ".countryName" $OBJECTTEMP)
-    EMOJI=$(jq -r ".countryEmoji" $OBJECTTEMP)
+    NAME=$(jq -r ".countryName" $OBJECTTEMP 2>/dev/null)
+    EMOJI=$(jq -r ".countryEmoji" $OBJECTTEMP 2>/dev/null)
     echo "$IP: $EMOJI $NAME"
-done < "${1:-/dev/stdin}"
+# done < <<<"${1}"
 sed -i "$ d" $TEMP
 echo "}" >> $TEMP
 
